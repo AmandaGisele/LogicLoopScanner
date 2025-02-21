e14b34f90986ae9c6813d62d3a25c00fe39494bd
AmandaGisele <126112724+AmandaGisele@users.noreply.github.com>
2025-02-20 22:23:13 -0700

    Update wifi.go

--- wifi.go
+++ wifi.go
@@ -3,12 +3,15 @@ package main
 import (
 	log "github.com/cihub/seelog"
 	"github.com/schollz/wifiscan"
+	"fmt"
 )
 
 func scanWifi(out chan map[string]map[string]interface{}) {
 	datas := make(map[string]map[string]interface{})
 	datas["wifi"] = make(map[string]interface{})
 	wifis, err := wifiscan.Scan(wifiInterface)
+	fmt.Println("Wifi porra")
+	fmt.Println(wifis)
 	if err != nil {
 		log.Error(err)
 	}
